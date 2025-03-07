f8ca3101a5815807d67d8797b5b043acdd941b93
Whatever90 <bazetug@gmail.com>
2025-03-07 00:03:00 -0800

    dev/dogs-storage+image-modal

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,7 +14,7 @@ import { PaginationComponent } from './pagination/pagination.component';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements AfterViewInit  {
   breeds: string[] = [];
   selectedBreeds: string[] = [];
   dogs: Dog[] = [];
@@ -27,19 +27,19 @@ export class SearchComponent implements OnInit {
   dogsTotalCount: number = 0;
   pagesTotalCount: number = 0;
   availableIndexes = [];
-  dogsIds: {[key: string]: any} = {};
+  dogsIds: { [key: string]: any } = {};
+  selectedDog: Dog | null = null;
   constructor(private dogService: DogService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     // this.fetchDogs();
     // Subscribe to query param changes
     this.route.queryParams.subscribe(params => {
-      console.log('SearchComponent params =>', params)
-      if(!params['breeds']) {
+      if (!params['breeds']) {
         this.selectedBreeds = [];
-      } else if(typeof params['breeds'] == 'string') {
+      } else if (typeof params['breeds'] == 'string') {
         this.selectedBreeds = [params['breeds']]
-      } else { 
+      } else {
         this.selectedBreeds = params['breeds']
       }
       this.sortOrder = params['sortOrder'] || 'asc';
@@ -47,22 +47,32 @@ export class SearchComponent implements OnInit {
       this.fetchDogs();
     });
   }
-
+  ngAfterViewInit(): void {
+    const modalElement = document.getElementById('dogImageExpandModal');
+    if (modalElement) {
+      modalElement.addEventListener('hidden.bs.modal', () => {
+        // If the active element is inside the modal, remove focus from it.
+        const activeElement = document.activeElement as HTMLElement;
+        if (activeElement && modalElement.contains(activeElement)) {
+          activeElement.blur();
+        }
+      });
+    }
+  }
   fetchDogs(): void {
-    console.log('fetchDogs', this.selectedBreeds, this.page, this.sortOrder, this.pageSize)
     this.loadingStatus = true;
     this.dogs = [];
     this.dogService.searchDogs(this.selectedBreeds, this.page, this.sortOrder, this.pageSize).subscribe({
       next: async data => {
-        if(data.total != this.dogsTotalCount) {
+        if (data.total != this.dogsTotalCount) {
           this.dogsTotalCount = data.total;
-          this.pagesTotalCount = Math.ceil(this.dogsTotalCount/this.pageSize);
+          this.pagesTotalCount = Math.ceil(this.dogsTotalCount / this.pageSize);
         }
         await this.getDogs(data.resultIds);
       },
       error: err => {
         console.error(err);
-        if(err.status == 401) {
+        if (err.status == 401) {
           this.router.navigate(['/'])
         }
         this.error = 'Failed to fetch dogs.';
@@ -70,21 +80,42 @@ export class SearchComponent implements OnInit {
     });
   }
   getDogs(ids: string[]): void {
-    this.dogService.getDogs(ids).subscribe({
+    this.dogs = [];
+    let dogsToPull: string[] = [];
+    let dogsToPullLocationIdx: { [key: string]: number } = {};
+    ids.forEach(id => {
+      if (this.dogsIds[id]) {
+        this.dogs.push(this.dogsIds[id]);
+      } else {
+        this.dogs.push({ id });
+        dogsToPull.push(id);
+        dogsToPullLocationIdx[id] = dogsToPull.length - 1;
+      }
+    });
+    if (!dogsToPull.length) {
+      this.loadingStatus = false;
+      return;
+    }
+    this.dogService.getDogs(dogsToPull).subscribe({
       next: data => {
-        this.dogs = data;
+        data.forEach(dog => {
+          this.dogs[dogsToPullLocationIdx[dog.id]] = dog;
+        })
         this.loadingStatus = false;
+        this.storeDogs(data);
       },
       error: err => {
         console.error(err);
-        if(err.status == 401) {
+        if (err.status == 401) {
           this.router.navigate(['/'])
         }
         this.error = 'Failed to pull dogs list.';
       }
     })
   }
-
+  storeDogs(dogs: Dog[]): void {
+    dogs.forEach(dog => this.dogsIds[dog.id] = dog);
+  }
   toggleFavorite(id: string): void {
     if (this.favorites.has(id)) {
       this.favorites.delete(id);
@@ -94,10 +125,10 @@ export class SearchComponent implements OnInit {
   }
 
   goToPage(page: number | string) {
-      this.page = (page as number);
-      this.fetchDogs();
+    this.page = (page as number);
+    this.fetchDogs();
   }
- 
+
 
   generateMatch(): void {
     const favArray = Array.from(this.favorites);
